84bfeb84434a466af4a5c0ba3ea7b55a069f8cca
Toprak <toprak1771@gmail.com>
2025-01-30 18:36:45 +0300

    port change

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   app.setGlobalPrefix('/api');
 
   app.useGlobalPipes(new ValidationPipe({whitelist:true,forbidNonWhitelisted:true}));
- 
-  await app.listen(process.env.PORT ?? 3000);
+  app.enableCors()
+  await app.listen(process.env.PORT ?? 5000);
 }
 bootstrap();
